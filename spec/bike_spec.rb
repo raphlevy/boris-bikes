c698e5c126e0b8248de3c497b31717156d295686
raphlevy <raphlevy.perso@gmail.com>
2015-07-22 11:32:39 +0100

    Started ex 10

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/bike'
 
 describe Bike do
 it 'test the Bike class' do
- should respond_to :working?
+ expect(subject).to  respond_to :working?
 end   
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,12 @@
 # spec/docking_station_spec.rb
 require_relative '../lib/docking_station'
 
+require_relative '../lib/bike'
+
 describe DockingStation do
-  it { should respond_to :release_bike}
+  it { is_expected.to respond_to :release_bike}
+end
+
+describe Bike do
+  it { is_expected.to respond_to :working?}
 end
