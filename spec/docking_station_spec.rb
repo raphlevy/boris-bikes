347a96ef26275ae06fb29c78f55849e41a1b934b
Haroon <haroon.hassan@gmail.com>
2015-07-23 17:21:29 +0100

    adjusted the last commit slightly

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,20 +15,9 @@ describe DockingStation do
       expect{subject.release_bike}.to raise_error 'No bikes available'
     end
   end
-  
+
   it { is_expected.to respond_to(:dock).with(1).argument}
 
-  describe '#dock' do
-     it 'checking if station is full' do
-     DockingStation::DEFAULT_CAPACITY.times {subject.dock Bike.new}
-     expect {subject.dock Bike.new}.to raise_error "Station full"
-   end
 
-   describe '#capacity' do
-      it 'does each station have a capacity' do
-        expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-      end
-     end
 
- end
 end
