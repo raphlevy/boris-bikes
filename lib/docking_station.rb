6f4a549b7061e87f7ef5f8793e669225e23108d7
Haroon <haroon.hassan@gmail.com>
2015-07-23 17:06:54 +0100

    End of redoing challenge 2

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,36 @@ require_relative 'bike'
 
 class DockingStation
 
+DEFAULT_CAPACITY = 20
+attr_reader :capacity
+
+
+def initialize
+	@bike = []
+	@capacity = DEFAULT_CAPACITY
+end
+ 
 
   def release_bike
-  	 raise 'No bikes available' unless @bike
-    @bike
+  	 raise 'No bikes available' if empty?
+    @bike.pop
   end
 
   def dock bike
   	 
-  	 fail "Station full" if @bike
-  	@bike = bike 
+  	fail "Station full" if full?
+  	@bike << bike
 
   end
+
+
+	private
+
+	def empty?
+		@bike.size == 0
+	end
+
+	def full?
+		@bike.size >= DEFAULT_CAPACITY
+	end
 end
