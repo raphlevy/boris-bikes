dcd32fdd0463f9db78761cd28a870fe72cfce03a
raphlevy <raphlevy.perso@gmail.com>
2015-07-24 13:27:03 +0100

    Finished post challenge 4

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,9 +5,9 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
   it 'releases working bike' do
-    subject.dock Bike.new
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = double :bike, broken?: false
+    subject.dock bike
+    expect(subject.release_bike).to be bike 
   end
 
   describe '#release_bike' do
@@ -16,9 +16,8 @@ describe DockingStation do
     end
 
     it "will not release_bike when it is broken" do
-    bike = Bike.new  
-    bike.report_broken
-    subject.dock (bike)  
+    bike = double :bike, broken?: true
+    subject.dock bike  
     expect{subject.release_bike}.to raise_error 'The bike is broken and cannot be released'  
     end
 
@@ -28,8 +27,8 @@ describe DockingStation do
 
   describe '#dock' do
     it "test if station is full" do
-      subject.capacity.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'docking station is full'  
+      subject.capacity.times { subject.dock double :bike}
+      expect { subject.dock double :bike }.to raise_error 'docking station is full'  
     end
   
     it "has a capacity" do
